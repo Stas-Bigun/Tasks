63934662e081bd22df88a9e8e8595fde66ad2621
Stas-Bigun <37585069+Stas-Bigun@users.noreply.github.com>
2018-03-26 21:10:54 +0300

    [Tasks] Added function fullname

---
+++ fullname.py
@@ -0,0 +1,8 @@
+
+def full_name():
+	name = input("Enter your name\n")
+	surname = input("Enter your surname\n")
+	print ("Your full name is " + name +" " + surname)
+
+full_name()
+full_name()			
--- sum.py
+++ sum.py
@@ -4,4 +4,5 @@ def sum(arg1, arg2):
 a = int(input())
 b = int(input())
 
+
 print (sum(a, b))
