b89a6fd74d314f020657804d705c54ccae880934
Stas-Bigun <37585069+Stas-Bigun@users.noreply.github.com>
2018-03-28 16:47:36 +0300

    [Tasks] Added div3 and div_con

---
+++ div3.py
@@ -0,0 +1,5 @@
+i = 0
+def div(arg1):
+	if arg1 %3 != 0:
+		return arg1 = arg1 + 1
+print (div(i))
---
+++ div_con.py
@@ -0,0 +1,6 @@
+i = int(input))
+def div(arg1):
+	if arg1 %3 != 0:
+		return arg1 = arg1 + 1
+	
+print (div(i))
